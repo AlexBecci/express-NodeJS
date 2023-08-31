b2150d86ab5e3bd360a0d929eeac94a88fe1dade
AlexBecci <beccialex2002@gmail.com>
2023-08-31 12:47:33 -0300

    morgan

--- index.js
+++ index.js
@@ -1,10 +1,34 @@
 const express = require("express");
+const morgan = require('morgan');
 
 const app = express();
+
+
+
+//middlewares
+//app.use(morgan('dev'));
+//app.use(morgan('short'));
+app.use(morgan('combined'));
+
+
+
+
+//routes
 
 app.get("/", (req, res) => {
   res.end("Hello world");
 });
+
+app.get("/login", (req, res) => {
+  res.end("Login");
+});
+
+app.get("*", (req, res) => {
+  res.end("Error 4040 not found");
+});
+
+
+
 app.listen(3000, () => {
   console.log("server on port 3000");
 });
